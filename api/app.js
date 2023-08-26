92e3dc7f1bb468cb64e9a7f60f0cf68b414ff5ea
Luís Farias <luisgmfarias@gmail.com>
2023-08-26 02:20:54 -0300

    feat: add api first commit

---
+++ api/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = 4000;
+
+
+app.get("/universities", async (req, res) => {
+  try {
+    const country = req.query.country;
+    const apiURL = `http://universities.hipolabs.com/search?country=${country}&limit=50`;
+
+    const response = await axios.get(apiURL);
+    const data = response.data;
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port 4000");
+});
